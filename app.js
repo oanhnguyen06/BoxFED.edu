ced4787a6440b91039f30f8b8f53b9adba91b645
oanhnguyen06 <oanhnguyen20649@gmail.com>
2025-11-26 19:45:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -841,7 +841,7 @@ async function sendChat() {
     addMessage(msg, "user");
 
     try {
-        const res = await fetch("http://localhost:3000/chat", {
+        const res = await fetch("http://localhost:11434/v1", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message: msg })
