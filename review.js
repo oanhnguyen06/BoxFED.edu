ca852e5623a8b0156764f481a623a6391e9d2d83
oanhnguyen06 <oanhnguyen20649@gmail.com>
2025-12-28 18:45:22 +0700

    Update review.js

--- review.js
+++ review.js
@@ -7,8 +7,10 @@ document.getElementById("surveyForm").addEventListener("submit", e => {
     score[i.value]++;
   });
 
-  document.getElementById("result").style.display = "block";
+  const email = document.getElementById("email").value;
 
+  // VẼ BIỂU ĐỒ
+  document.getElementById("result").style.display = "block";
   new Chart(document.getElementById("radarChart"), {
     type: "radar",
     data: {
@@ -16,12 +18,24 @@ document.getElementById("surveyForm").addEventListener("submit", e => {
       datasets: [{
         data: [score.V, score.A, score.R, score.K],
         backgroundColor: "rgba(22,62,156,0.2)",
-        borderColor: "#163e9c",
-        borderWidth: 2
+        borderColor: "#163e9c"
       }]
     },
-    options: {
-      scales: { r: { beginAtZero: true } }
-    }
+    options: { scales: { r: { beginAtZero: true } } }
   });
+
+  //  n8n
+  fetch("https://oanhnguyen.app.n8n.cloud/webhook-test/vark-survey", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email,
+      V: score.V,
+      A: score.A,
+      R: score.R,
+      K: score.K
+    })
+  })
+  .then(() => alert("Đã gửi kết quả về email!"))
+  .catch(() => alert("Lỗi gửi email"));
 });
