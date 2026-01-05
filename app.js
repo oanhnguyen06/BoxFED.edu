cece7e2d3cdc03710158f6236aa5c19328cd88c3
oanhnguyen06 <oanhnguyen20649@gmail.com>
2026-01-05 07:56:04 +0700

    Update name for TS. Vũ Đình Minh

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ const lecturers = [
 {
   "key": "vu_dinh_minh",
   "img": "img/thay-minh.jpg",
-  "name": "TS. Vũ Đình Minh",
+  "name": "TS. MINH VUC ĐÌNH",
   "title": "Giảng viên",
   "dept": "Bộ môn Sư phạm các ngành kỹ thuật, Viện Sư phạm Kỹ thuật, Đại học Bách Khoa Hà Nội",
   "office": "M329, Nhà C7",
